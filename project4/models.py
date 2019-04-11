3305af78e97f331c941f2bd223a5d22c5b9d70f7
Leonard Busenah <busenah@gmail.com>
2019-04-11 13:12:37 -0400

    created models and database

--- project4/models.py
+++ project4/models.py
@@ -6,8 +6,6 @@ from django.db import models
 class Doctor(models.Model):
     name = models.CharField(max_length=100)
     specialty = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=100)
     photo_url = models.TextField()
 
     def __str__(self):
@@ -17,13 +15,9 @@ class Doctor(models.Model):
 class Patient(models.Model):
     doctor = models.ForeignKey(
         Doctor, on_delete=models.CASCADE, related_name='patients')
-    name = models.CharField(max_length=100, default="Patient's name")
-    phone_number = models.CharField(max_length=100, default="patient's phone number")
-    address = models.CharField(max_length=100, default="Patient's physical address")
-    symptoms = models.CharField(max_length=100, default='Description or Symtpoms')
     time = models.CharField(max_length=100, default='no preferred time')
     date = models.CharField(max_length=100, default='no preferred date')
-    doctor_to_see = models.CharField(max_length=200, null=True)
+    preview_url = models.CharField(max_length=200, null=True)
 
     def __str__(self):
-        return self.name
+        return self.title
