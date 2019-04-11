ca4ee16d87310f24ae64af7e044b66c4099f3be8
Leonard Busenah <busenah@gmail.com>
2019-04-11 18:30:29 -0400

    fexperimenting with models.py

--- project4/models.py
+++ project4/models.py
@@ -15,8 +15,16 @@ class Doctor(models.Model):
 class Patient(models.Model):
     doctor = models.ForeignKey(
         Doctor, on_delete=models.CASCADE, related_name='patients')
+    title = models.CharField(max_length=100, default="patient's name")
+    # name = models.CharField(max_length=100, default="")
+    phone_number = models.CharField(max_length=100, default="patient's phone number")
+    address = models.CharField(
+        max_length=100, default="patient's physical address")
+    next_of_kin = models.CharField(
+        max_length=100, default="patient's next of kin")
+
     time = models.CharField(max_length=100, default='no preferred time')
-    date = models.CharField(max_length=100, default=' / /')
+    date = models.CharField(max_length=100, default='')
     description = models.CharField(max_length=200, default='')
 
     def __str__(self):
