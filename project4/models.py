8f6161a43552a345540b0649abc589bd2dce361e
Leonard Busenah <busenah@gmail.com>
2019-04-12 11:14:42 -0400

    added more fields in the models

--- project4/models.py
+++ project4/models.py
@@ -6,6 +6,10 @@ from django.db import models
 class Doctor(models.Model):
     name = models.CharField(max_length=100)
     specialty = models.CharField(max_length=100)
+    phone_number = models.CharField(
+        max_length=100)
+    address = models.CharField(
+        max_length=100)
     photo_url = models.TextField()
 
     def __str__(self):
@@ -20,12 +24,12 @@ class Patient(models.Model):
     phone_number = models.CharField(max_length=100, default="patient's phone number")
     address = models.CharField(
         max_length=100, default="patient's physical address")
-    next_of_kin = models.CharField(
+    date_of_Birth = models.CharField(max_length=100, default="patient's date of birth")
+    next_of_Kin = models.CharField(
         max_length=100, default="patient's next of kin")
-
-    time = models.CharField(max_length=100, default='no preferred time')
-    date = models.CharField(max_length=100, default='')
-    description = models.CharField(max_length=200, default='')
+    time = models.CharField(max_length=100, default="time of appointment")
+    date = models.CharField(max_length=100, default="today's date")
+    description = models.CharField(max_length=200, default='description, or symptoms of illness')
 
     def __str__(self):
         return self.title
